27b5f331cc3a3fda4e4857533ace01017a7a9f1e
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-01-01 21:08:29 -0600

    Router system

---
+++ src/router/AppRouter.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Redirect,
+    BrowserRouter as Router,
+     Switch, 
+     Route} from 'react-router-dom'
+import { LoginScreen } from '../components/auth/LoginScreen'
+import { CalendarScreen } from '../components/calendar/CalendarScreen'
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/login" component={LoginScreen}/>
+                    <Route exact path="/" component={CalendarScreen}/>
+
+                    <Redirect to="/"/>
+                </Switch>
+            </div>
+        </Router>
+    )
+}
